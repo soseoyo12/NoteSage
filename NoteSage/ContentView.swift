12e785283ec76d3ac35c472ff48c397d4137c294
soseoyo12 <songsy0612@gmail.com>
2025-12-13 00:23:45 +0900

    Initial commit

---
+++ NoteSage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteSage
+//
+//  Created by 송성용 on 12/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteSage/NoteSageApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteSageApp.swift
+//  NoteSage
+//
+//  Created by 송성용 on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteSageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
